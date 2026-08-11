248904890a5679e9834260cacd77d768f19fa349
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-11 21:25:40 +0530

    comments for length based

--- length_based.py
+++ length_based.py
@@ -1,16 +1,23 @@
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader('dl-curriculum.pdf')
 
+# Load the PDF
+loader = PyPDFLoader('dl-curriculum.pdf')
 docs = loader.load()
 
+
+# Split text into fixed-size chunks
 splitter = CharacterTextSplitter(
-    chunk_size=200,
-    chunk_overlap=0,
-    separator=''
+    chunk_size=200,       # Maximum characters per chunk
+    chunk_overlap=0,     # No overlapping characters
+    separator=''          # Split directly by characters
 )
 
+
+# Split each Document into smaller Documents
 result = splitter.split_documents(docs)
 
+
+# Print the first chunk
 print(result[0].page_content)
